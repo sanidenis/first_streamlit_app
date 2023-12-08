11792b228e4febd74bfc32dc41a1fec32c48bed2
sanidenis <153252692+sanidenis@users.noreply.github.com>
2023-12-07 20:09:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
+# Import python packages
+import streamlit
+from snowflake.snowpark.context import get_active_session
 
+# Write directly to the app
+streamlit.title("My Parents' New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.write("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.write("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.write("🐔 Hard-Boiled Free-Range Egg")
+streamlit.write("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
