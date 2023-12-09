eb59e06d8e871e022cd4087656f3c1e172f6b371
sanidenis <153252692+sanidenis@users.noreply.github.com>
2023-12-08 17:18:58 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice);
+def get_fruityvice_data(this_fruit_choice)
   #run panda's normalize function on the json response
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   #make a new table withthe normalized response
