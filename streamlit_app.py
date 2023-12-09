3c48d9b1ab3f826f900177f80eb76939d5e6e54c
sanidenis <153252692+sanidenis@users.noreply.github.com>
2023-12-08 17:18:18 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,13 @@ fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice);
+  #run panda's normalize function on the json response
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  #make a new table withthe normalized response
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.header('Fruityvice Fruit Advice!')
 
 try:
@@ -29,14 +36,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    streamlit.write('The user entered ', fruit_choice)
-    
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    
-    #run panda's normalize function on the json response
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    #make a new table withthe normalized response
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
